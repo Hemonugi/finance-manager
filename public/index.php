3408bb4d79cf5761cf21ccdf25fcd8b528e0a085
hemonugi <hemonugi@gmail.com>
2023-07-08 18:22:09 +0400

    Added configuration file

--- public/index.php
+++ public/index.php
@@ -3,13 +3,18 @@
 declare(strict_types=1);
 
 use DI\Bridge\Slim\Bridge;
+use DI\ContainerBuilder;
 use Hemonugi\FinanceManager\Account\AccountController;
 use Hemonugi\FinanceManager\Swagger\SwaggerController;
 use Middlewares\TrailingSlash;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = Bridge::create();
+$builder = new ContainerBuilder();
+$builder->addDefinitions(__DIR__ . '/../configs/settings.php');
+$container = $builder->build();
+
+$app = Bridge::create($container);
 $app->add((new TrailingSlash(true))->redirect());
 
 $app->get('/api/doc/config/', [SwaggerController::class, 'config']);
